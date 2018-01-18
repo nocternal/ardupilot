734fc5ba8ccd0ef99a1cddaba310030cdd8ddef7
nocternal <570818564@qq.com>
2018-01-18 15:32:46 +0800

    改动fg_plnae_view.bat 飞机3D模型换为BWB3

--- Tools/autotest/fg_plane_viewOriginal.bat
+++ Tools/autotest/fg_plane_viewOriginal.bat
@@ -1,15 +1,14 @@
 set AUTOTESTDIR="%~dp0\aircraft"
 
-set FGDIR=FlightGear 2017.1.2
+FOR /F "delims=" %%D in ('dir /b "\Program Files"\FlightGear*') DO set FGDIR=%%D
 echo "Using FlightGear %FGDIR%"
-D:
-cd "D:\Program Files\%FGDIR%\bin"
+cd "\Program Files\%FGDIR%\bin"
 fgfs ^
     --native-fdm=socket,in,10,,5503,udp ^
     --fdm=external ^
     --aircraft=Rascal110-JSBSim ^
     --fg-aircraft=%AUTOTESTDIR% ^
-    --airport=TNCS ^
+    --airport=CMAC ^
     --geometry=650x550 ^
     --bpp=32 ^
     --disable-anti-alias-hud ^
